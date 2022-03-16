f8cfaa384b03ce02a6ca2d8fa12db4d68050b17e
Udayadroit <uday.adroit1@gmail.com>
2022-03-16 12:43:10 +0530

    add creation of list

---
+++ list.py
@@ -0,0 +1,12 @@
+# python program to illustrate list
+# creation of empty list
+
+nums= []
+
+#apending data into the list
+nums.append(21)
+nums.append(22)
+nums.append(23.5)
+nums.append("string")
+
+print(nums)
