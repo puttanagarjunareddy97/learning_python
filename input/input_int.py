5ef2f410e6e72a3f78dfed16da83070671da8688
Udayadroit <uday.adroit1@gmail.com>
2022-03-16 13:51:08 +0530

    add intrger input

---
+++ input/input_int.py
@@ -0,0 +1,8 @@
+# python program to get input from user
+#accepting integer from user
+#the return type of input() function is string
+# so we need to convert the input to integer
+num_1=int(input("enter numer"))
+num_2=int(input("enter the second number"))
+num_3=num_1*num_2
+print("the multiply is",num_3)
