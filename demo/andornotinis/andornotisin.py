f55c328434442b001915465732f2b4416c56c46d
Udayadroit <uday.adroit1@gmail.com>
2022-03-19 13:41:21 +0530

    iterative programs

---
+++ demo/andornotinis/andornotisin.py
@@ -0,0 +1,27 @@
+#logical operators
+#or (returns true)
+print(True or False)
+
+# and (returns false)
+print(True and False )
+
+# not (returns false)
+print(not False)
+
+# using in to check 
+if 's' in 'suraj kumar ':
+    print('s is a part of the name ')
+else :
+    print ('S is a not part of your name')
+for i in 'suraj kumar' :
+    print(i,end=' ')
+
+print('\r') 
+# using is to check object identity 
+# string is imuttable (cannot be changed once allocated )
+# hence occupy the same memory allocation 
+print(' 'is' ')
+# using is to check object identity 
+# dictinory  is muttable (cannot be changed once allocated )
+# hence occupy the same memory allocation 
+print({ } is { })
