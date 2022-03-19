7129b151d8d91fb4842a9c993a82a15e30b7b84d
Udayadroit <uday.adroit1@gmail.com>
2022-03-19 14:28:48 +0530

    conditional keywords

---
+++ ifelifelse/ifelifelse.py
@@ -0,0 +1,9 @@
+# program for if ,elif,else
+age=int(input("emter the age"))
+
+if age <13 or age<19 :
+    print("child age or teen age")
+elif age>20:
+    print("youngage")
+else :
+    print("old age")
